0b514e9666834e5dac28d50d0876aa86fbec73ff
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-12-07 14:45:59 +0100

    Add tag and properly define article

--- app/components/article_component.rb
+++ app/components/article_component.rb
@@ -2,29 +2,45 @@
 
 class ArticleComponent < ViewComponent::Base
 
+  renders_many :tags, "TagComponent"
+
   erb_template <<-ERB
     <%= render(CardComponent.new(additional_classes: 'article')) do %>
       <%= tag.div(class: 'article--images') do %>
-        <%= image_tag("https://lcp.fr/sites/lcp.fr/files/styles/entete_contenu_1030x570/public/2024-09/Macron%202.jpg?h=dd98df0d&itok=WtCAyrGE", class: "article--main-image")%>
-        <%= image_tag("https://www.abilitis.fr/wp-content/uploads/2024/11/franceinfo-en-journee-speciale-A-quoi-ressemblera-votre-ete-_-vendredi-1er-juillet.jpg", class: "article--source-logo-image")%>
+        <%= image_tag(@image_url, class: "article--main-image")%>
+        <% if @source_logo_url %>
+          <%= image_tag(@source_logo_url, class: "article--source-logo-image")%>
+        <% end %>
         
       <% end %>
       <%= tag.div(class: "article--content") do %>
         <%= tag.div(class: "article--content-body") do %>
-          <%= tag.h3(@title, class: "article--title") %>
+          <%= link_to(@image_url, class: "article--image") do %>
+            <%= tag.h3(@title, class: "article--title") %>
+          <% end %>
           <%= tag.p do%>
             <%= content %>
           <% end %>
         <% end %>
         <%= tag.div(class: "article--content-footer") do %>
-          <%= tag.div("Tags", class: "article--tags") %>
-          <%= tag.p("1j - FranceInfo", class: "article--information") %>
+          <%= tag.div(class: "article--tags") do %>
+            <% if tags.any? %>
+              <% tags.each do |tag| %>
+                <%= tag %>
+              <% end %>
+            <% end %>
+          <% end %>
+          <%= tag.p(@published + " - " + @source, class: "article--information") %>
         <% end %>
       <% end %>
     <% end %>
   ERB
 
-  def initialize(title:)
+  def initialize(title:, image_url:, published:, source:, source_logo_url: nil)
     @title = title
+    @image_url = image_url
+    @published = published
+    @source = source
+    @source_logo_url = source_logo_url
   end
 end
--- test/components/previews/article_component_preview.rb
+++ test/components/previews/article_component_preview.rb
@@ -1,6 +1,13 @@
 class ArticleComponentPreview < Lookbook::Preview
   def default
-    render ArticleComponent.new(title: "Vague de chaleur et canicule la semaine prochaine") do |article|
+    render ArticleComponent.new(
+      title: "Vague de chaleur et canicule la semaine prochaine",
+      image_url: 'https://media.ouest-france.fr/v1/pictures/MjAyNDA1ZmQ4MjVlMjQ5NTkzMzRkMjVjN2NmNTE2NzQ3ZGI3ZTE?width=375&focuspoint=50%2C25&cropresize=1&client_id=bpeditorial&sign=6366dc112457c7774031937f96e0e4b8264881d530e007d3578f0c447f1fc6ef',
+      published: "1j",
+      source: "FranceInfo",
+      source_logo_url: "https://www.abilitis.fr/wp-content/uploads/2024/11/franceinfo-en-journee-speciale-A-quoi-ressemblera-votre-ete-_-vendredi-1er-juillet.jpg"
+      ) do |article|
+      article.with_tags([{text: "Tag 1", path: "/tags/1"}, {text: "Politics", path: "/tags/2"}])
       "Après un début d’été parfois mitigé sur la France, l’air brulant en embuscade sur le bassin méditerranéen s’étendra brusquement sur le territoire la semaine prochaine. Avec jusqu’à 40°C envisagés sur nos thermomètres, cette vague de chaleur nationale pourrait devenir caniculaire dans le sud de la France."
     end
   end
